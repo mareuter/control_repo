b0d74b92ffb7d5a984befe26c7e482e5636d40cb
Michael Reuter <mareuternh@gmail.com>
2019-07-23 20:34:36 -0700

    Update ssh_clients.pp

--- site/profile/manifests/ssh_clients.pp
+++ site/profile/manifests/ssh_clients.pp
@@ -1,8 +1,8 @@
 class profile::ssh_clients {
   host {'db_server':
-    ip: '172.18.0.3',
+    ip => '172.18.0.3',
   }
   host {'web_server':
-    ip: '172.18.0.2',
+    ip => '172.18.0.2',
   }
 }
